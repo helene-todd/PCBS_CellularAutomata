30d6ee5b3085834e5345b41a162bf9999106ff25
Helen <helen.thedove@gmail.com>
2020-01-19 03:11:09 +0100

    Added argparse arguments and screen captures

--- automaton.py
+++ automaton.py
@@ -6,16 +6,16 @@ random.seed()
 class Automata_list() :
 
     # Constructor for a display specified in text file
-    def __init__(self, grid, l, m):
+    def __init__(self, grid, l, m, rules):
         self.columns = len(grid[0])
         self.rows = len(grid)
         self.list = [[None]*self.columns for k in range(self.rows)]
         for row in range(self.rows):
             for col in range(self.columns):
                 if grid[row][col] == '1' :
-                    self.list[row][col] = Automaton(row, col, l, m, "alive")
+                    self.list[row][col] = Automaton(row, col, l, m, "alive", rules)
                 else :
-                    self.list[row][col] = Automaton(row, col, l, m, "dead")
+                    self.list[row][col] = Automaton(row, col, l, m, "dead", rules)
         self.count_neighbours()
 
 
@@ -34,29 +34,33 @@ class Automata_list() :
     def count_neighbours(self):
         for i in range(self.rows):
             for j in range(self.columns):
+                        nb_rows = len(self.list)
+        nb_columns = len(self.list[0])
+        for i in range(self.rows):
+            for j in range(self.columns):
                 # Right
-                if j < self.columns-1 and self.list[i][j+1] != None and self.list[i][j+1].isAlive():
+                if self.list[i][(j+1)%nb_columns].isAlive() :
                     self.list[i][j].neighbours += 1
                 # Left
-                if j > 0 and self.list[i][j-1] != None and self.list[i][j-1].isAlive():
-                    self.list[i][j].neighbours += 1
-                # Up
-                if i < self.rows-1 and self.list[i+1][j] != None and self.list[i+1][j].isAlive():
+                if self.list[i][j-1].isAlive() :
                     self.list[i][j].neighbours += 1
                 # Down
-                if i > 0 and self.list[i-1][j] != None and self.list[i-1][j].isAlive():
+                if self.list[(i+1)%nb_rows][j].isAlive() :
                     self.list[i][j].neighbours += 1
-                # Bottom Right
-                if j < self.columns-1 and i < self.rows-1  and self.list[i+1][j+1] != None and self.list[i+1][j+1].isAlive():
+                # Up
+                if self.list[i-1][j].isAlive() :
                     self.list[i][j].neighbours += 1
-                # Bottom Left
-                if j > 0 and i < self.rows-1 and self.list[i+1][j-1] != None and self.list[i+1][j-1].isAlive():
+                # Down Left
+                if self.list[(i+1)%nb_rows][(j+1)%nb_columns].isAlive() :
                     self.list[i][j].neighbours += 1
-                # Top Left
-                if j > 0 and i > 0 and self.list[i-1][j-1] != None and self.list[i-1][j-1].isAlive():
+                # Down Right
+                if self.list[(i+1)%nb_rows][j-1].isAlive() :
                     self.list[i][j].neighbours += 1
-                # Top Right
-                if j < self.columns-1 and i > 0 and self.list[i-1][j+1] != None and self.list[i-1][j+1].isAlive():
+                # Upper Right
+                if self.list[i-1][(j+1)%nb_columns].isAlive() :
+                    self.list[i][j].neighbours += 1
+                # Upper Left
+                if self.list[i-1][j-1].isAlive() :
                     self.list[i][j].neighbours += 1
 
 
@@ -67,11 +71,19 @@ class Automata_list() :
         self.reset_neighbours()
         self.count_neighbours()
 
+    def isAlive(self):
+        alive = False
+        for row in self.list:
+            for element in row:
+                if element.isAlive() :
+                    alive = True
+        return alive
+
 
 class Automaton(pygame.sprite.Sprite):
 
     # Automaton constructor
-    def __init__(self, row, column, length, margin, state):
+    def __init__(self, row, column, length, margin, state, rules):
         # Call superclass constructor
         super().__init__()
 
@@ -80,38 +92,49 @@ class Automaton(pygame.sprite.Sprite):
         # Size of Automaton square
         self.width, self.height = length, length
         self.state = state
+        self.rules = rules
 
         self.happy = False
-        self.sad = False
+
+        self.newborn = True
 
         self.neighbours = 0
 
+# TRY COLOURING NEWBORN IN WHITE/ CREME COLOUR
 
     def display(self, screen):
-        if self.state == "alive":
-            if self.neighbours < 2 or self.neighbours > 3:
-                self.sad = True
-                self.color = pygame.Color('#67D0DD')
+        if self.state == "alive" :
+            if self.neighbours < self.rules[0] or self.neighbours > self.rules[1]:
+                self.happy = False
+                if self.newborn != True :
+                    self.color = pygame.Color('#70eeff')
+                elif self.newborn == True :
+                    self.color = pygame.Color('#bcf7ff')
+                    self.newborn = False
                 self.image = pygame.image.load('sad_square.png').convert_alpha()
             else :
                 self.happy = True
-                self.color = pygame.Color('#DC95DD')
+                if self.newborn != True :
+                    self.color = pygame.Color('#fd8eff')
+                elif self.newborn == True :
+                    self.color = pygame.Color('#fed1ff')
+                    self.newborn = False
                 self.image = pygame.image.load('happy_square.png').convert_alpha()
+
             self.image = pygame.transform.smoothscale(self.image, (self.width, self.height))
             self.rect =  self.image.get_rect(left = self.x, top = self.y)
             pygame.draw.rect(screen, self.color, self.rect)
             screen.blit(self.image, self.rect)
 
-
     def update(self) :
-        if self.state == "alive" and self.sad == True:
+        if self.state == "alive" and self.happy == False:
             self.state = "dead"
-            self.happy, self.sad = False, False
+            self.newborn = False
 
         elif self.state == "dead" :
-            if self.neighbours == 3 :
+            if self.neighbours == self.rules[2] :
                 self.state = "alive"
-
+                self.newborn = True
 
     def isAlive(self):
         return self.state == "alive"
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
+from argparse import ArgumentParser
+from argparse import RawTextHelpFormatter
 from automaton import *
-import argparse
 import sys
 import os
 
@@ -10,33 +11,70 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (127, 127, 127)
 
-# Initialise pygame
-pygame.init()
 
-# Argparse for a file
-parser = argparse.ArgumentParser(description='Either a file or manually')
-parser.add_argument("LIST", help="Arguments stored in list, or just nothing", nargs="*")
+# Argparse arguments
+parser = ArgumentParser(description='''A simulation of Conway's Game of Life in a finite space. This program shows how cellular automaton evolve
+in a deterministic way depending on the starting grid and set of rules.''', formatter_class=RawTextHelpFormatter)
+parser.add_argument('-s', '--size',
+    help='specify size for randomly generated initial grid \n'
+    '  takes 2 integer arguments : row_size column_size', nargs=2, metavar='int', type=int)
+parser.add_argument('-p', '--proportion',
+    help='specify proportion of cells on initial grid \n'
+    '   takes 1 integer argument : proportion (between 0 and 1)', nargs=1, metavar='float', type=float)
+parser.add_argument('-f', '--file',
+    help='specify a text file as starting grid \n'
+    '   takes 1 argument : path to .txt file', nargs=1, metavar='path', type=str)
+parser.add_argument('-r', '--rules', help='specify rules of game\n'
+    '   takes 3 integer arguments x y z : \n'
+    '     - 1st argument x : cell dies if less than x neighbours\n'
+    '     - 2nd argument y : cell dies if more than y neighbours\n'
+    '     - 3rd argument z : cell is born if exactly z neighbours\n', nargs=3, metavar='int', type=int)
 args = parser.parse_args()
 
-if not any(vars(args).values()):
-    grid = [['1' if random.random() < 0.33 else "0" for j in range(12)] for j in range(12)]
-elif os.path.exists(args.LIST[0]):
-    with open(args.LIST[0], 'r') as f:
-        grid = [line.replace('\n','').split(' ') for line in f]
+# String that stores argparse values
+str_arguments = ""
+
+# Check arguments for initial grid
+if args.file is not None and os.path.exists(args.file[0]): #need to test on windows
+    str_arguments = f"File opened : {args.file[0]} \n"
+    with open(args.file[0], 'r') as f:
+        grid = [line.replace('\n','').split(' ') for line in f if line !='\n']
+elif args.size is not None :
+    str_arguments = f"File opened : None \n"
+    p = .33
+    if args.proportion is not None and args.proportion[0] >= 0 and args.proportion[0] <= 1 :
+        p = args.proportion[0]
+    grid = [['1' if random.random() < p else '0' for i in range(args.size[1])] for j in range(args.size[0])]
+    str_arguments += f"Proportion of cells : {p} \n"
+    str_arguments += f"Grid size : {args.size[0]} rows x {args.size[1]} columns \n"
 else :
-    print('Wrong arguments : please specify text file path (grids/file.txt) or nothing.')
-    sys.exit()
+    p = .33
+    if args.proportion is not None and args.proportion[0] >= 0 and args.proportion[0] <= 1 :
+        p = args.proportion[0]
+    grid = [['1' if random.random() < p else '0' for i in range(16)] for j in range(16)] # Default grid
+    str_arguments += f"Proportion of cells : {p} \n"
+    str_arguments += "Grid size : 16 rows x 16 columns \n"
+
+# Check arguments for special rules
+if args.rules is not None :
+    rules = [args.rules[0], args.rules[1], args.rules[2]]
+else :
+    rules = [2,3,3] # Default rules
+str_arguments += f"Rules : {rules} \n"
 
 
 # Number of columns/ rows
 columns = len(grid[0])
 rows = len(grid)
 
-# Length of one cell
-l = (int((4./5)*pygame.display.Info().current_h/max(columns, rows)))
+# Initialise pygame
+pygame.init()
+
+# Length of one cell depending on row to column ratio (to define an appropriate size on screen)
+l = min(int((4./5)*pygame.display.Info().current_h/rows), int((4./5)*pygame.display.Info().current_w/columns))
 # Margin (size of space between cells)
 m = (int(0.04*l))
-# Width, height of screen
+# Width, height of window
 H, W = rows*(l+m)+m, columns*(l+m)+m
 
 # Title of window
@@ -47,35 +85,42 @@ screen = pygame.display.set_mode((W, H), pygame.DOUBLEBUF)
 screen.fill(GRAY)
 
 # Initialise font to adapt to screen size
-font = pygame.font.Font('freesansbold.ttf', int(min(H,W)/20))
+font = pygame.font.Font('freesansbold.ttf', int(W/18))
 
 # Initialise the text
-text = font.render('Press [spacebar] to start / pause', True, BLACK)
-
+text = font.render('Press [space bar] to start / pause', True, BLACK)
 # Get the rectangle that contains the text
 textRect = text.get_rect()
-
 # Center text in middle of screen using rectangle
 textRect.center = (W // 2, H // 2)
-
 # Blit the text and rectangle together
 screen.blit(text, textRect)
-
 # Update Screen
 pygame.display.update()
 
-# List of automata
-automata_list = Automata_list(grid, l, m)
+
+# New list of automata
+automata_list = Automata_list(grid, l, m, rules)
 
 # Initialise pygame's clock
 clock = pygame.time.Clock()
 
-# Avoid errors
+# Avoid errors related to event
 pygame.event.clear()
 
 # Animation hasn't started yet
 start = False
 
+# Creating folder to save screen captures and used arguments
+if os.path.isdir('screen_captures') :
+    os.system("rm -rf screen_captures")
+os.mkdir(f'screen_captures')
+with open('screen_captures/optional_arguments.txt','w') as f:
+    f.write(str_arguments)
+
+
+i = 0
+bound = columns*rows
 while True:
 
     # Clock delay
@@ -98,3 +143,7 @@ while True:
         automata_list.display(screen)
         pygame.display.update()
         automata_list.update()
+
+        if i <= bound :
+            pygame.image.save(screen, f"screen_captures/{i}.jpeg")
+            i += 1
